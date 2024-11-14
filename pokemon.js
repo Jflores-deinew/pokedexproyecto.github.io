7eb91f3f85ef812f9d022d48848e6ee53cbb8044
Jflores-deinew <juangregorioarcos@gmail.com>
2024-11-14 15:47:19 -0400

    tres

---
+++ pokemon.js
@@ -0,0 +1,25 @@
+const MAX_POKEMON = 151;
+const listWrapper = document.querySelector(".list-wrapper");
+const searchInput = document.querySelector("#search-input");
+const numberFilter = document.querySelector("#number");
+const nameFilter = document.querySelector("#name");
+const notFoundMessage = document.querySelector("#not-found-message");
+let allPokemons = [];
+fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`).then((response) => response.json()).then((data) => { allPokemons = data.results; displayPokemons(allPokemons);});
+async function fetchPokemonDataBeforeRedirect(id) {
+  try {
+  const [pokemon, pokemonSpecies] = await Promise.all([
+   fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) =>
+     res.json()
+   ),
+   fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) =>
+     res.json()
+   ),
+  ]);
+   return true;
+  } catch (error) {
+   console.error("No se pudieron recuperar los datos del Pokémon");
+  }
+}
+	
+
